beca872810ee77951b812c285259d86840ba7087
Tanishqsoni <tanishqsoni3112@gmail.com>
2021-06-18 16:06:55 +0530

    Revert "firebase connection not working"
    This reverts commit 05beac61e5b458d629bbd055955475e59c67e47e.

--- backend_2/firebase_connect.js
+++ backend_2/firebase_connect.js
@@ -4,7 +4,7 @@ const app = firebase.initializeApp({
     apiKey: "AIzaSyDGBFLQF3afGH17DWYdRn7i5qHGh_9pg2o",
     authDomain: "cfg-2021-team-29.firebaseapp.com",
     databaseURL: "https://cfg-2021-team-29-default-rtdb.firebaseio.com",
-    projectId: "cfg-2021-team-29",
+
 })
 
 module.exports  = app;
